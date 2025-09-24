99b0ad47267cb0db6666ea53d82b375ef154006d
NefiPineda <nefipinedagal@gmail.com>
2025-09-24 12:07:52 -0600

    Actualizar la base de la configuración de Vite a "/Practica1-N2/"

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/practica_1/',
+  base: "/Practica1-N2/",
 });
